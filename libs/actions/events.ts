4e479d092676bb3ae3ae44c50c0148dd09781d32
Jose Guzman <joseguzmandev@gmail.com>
2025-03-22 13:39:20 -0500

    refactor: revalidate path

--- libs/actions/events.ts
+++ libs/actions/events.ts
@@ -3,53 +3,63 @@
 import { EventFieldTypes } from '@/types/event';
 import { OrderAtomTypes } from '../atoms/order';
 import { createEvent, editEvent } from '../services/events';
-import { handleActionError, printErrorsOnDevEnv } from '../serializers/error-handler';
+import {
+  handleActionError,
+  printErrorsOnDevEnv,
+} from '../serializers/error-handler';
 import { redirect } from 'next/navigation';
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { revalidatePath } from 'next/cache';
 
 export async function createEventAction(
-   orders: OrderAtomTypes[],
-   prevState: any,
-   formData: FormData
+  orders: OrderAtomTypes[],
+  prevState: any,
+  formData: FormData,
 ) {
-   const data = Object.fromEntries(formData.entries()) as Record<EventFieldTypes, string>;
+  const data = Object.fromEntries(formData.entries()) as Record<
+    EventFieldTypes,
+    string
+  >;
 
-   try {
-      await createEvent({
-         name: data.name,
-         date: new Date(data.date).toISOString(),
-         location: data.location,
-         description: data.description,
-         orders,
-      });
-   } catch (error) {
-      printErrorsOnDevEnv(error);
-      return handleActionError(error, data);
-   }
-   redirect('/');
+  try {
+    await createEvent({
+      name: data.name,
+      date: new Date(data.date).toISOString(),
+      location: data.location,
+      description: data.description,
+      orders,
+    });
+  } catch (error) {
+    printErrorsOnDevEnv(error);
+    return handleActionError(error, data);
+  }
+  revalidatePath('/');
+  redirect('/');
 }
 
 export async function editEventAction(
-   id: string,
-   orders: OrderAtomTypes[],
-   prevState: any,
-   formData: FormData
+  id: string,
+  orders: OrderAtomTypes[],
+  prevState: any,
+  formData: FormData,
 ) {
-   const data = Object.fromEntries(formData.entries()) as Record<EventFieldTypes, string>;
+  const data = Object.fromEntries(formData.entries()) as Record<
+    EventFieldTypes,
+    string
+  >;
 
-   try {
-      await editEvent({
-         id,
-         name: data.name,
-         date: new Date(data.date).toISOString(),
-         location: data.location,
-         description: data.description,
-         orders,
-      });
-   } catch (error) {
-      printErrorsOnDevEnv(error);
-      return handleActionError(error, data);
-   }
-   redirect('/');
+  try {
+    await editEvent({
+      id,
+      name: data.name,
+      date: new Date(data.date).toISOString(),
+      location: data.location,
+      description: data.description,
+      orders,
+    });
+  } catch (error) {
+    printErrorsOnDevEnv(error);
+    return handleActionError(error, data);
+  }
+  revalidatePath('/');
+  redirect('/');
 }
